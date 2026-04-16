4385599cc2315ebda471f9276f645cc3ab5afe3c
Aditya Kumar <plovers390@gmail.com>
2026-04-16 05:52:24 +0530

    Change API_URL to use '/remote/api'

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { toast } from 'react-hot-toast';
 import { store } from '../store/store';
 import { startLoading, stopLoading } from '../store/slices/uiSlice';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || '/remote/api';
 
 const api = axios.create({
   baseURL: API_URL,
